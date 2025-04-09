9b27f13281a78a9c44e4f9297d1183893fcec963
LuisBasto <luisbasto@proton.me>
2025-04-09 16:42:23 +0100

    Versão 2.2

---
+++ ProjectoGestaoBiblioteca/Copy.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectoGestaoBiblioteca
+{
+    internal class Copy
+    {
+        public string Code { get; set; }
+        public Book Book { get; set; }
+        public int Edition { get; set; }
+        public int EditionYear { get; set; }
+        public bool IsLoaned { get; set; }
+        public Condition Condition { get; set; }
+        public DateTime? LoanDate { get; set; }
+
+        public Copy(string code, Book book, int edition, int editionYear, Condition condition)
+        {
+            Code = code;
+            Book = book;
+            Edition = edition;
+            EditionYear = editionYear;
+            IsLoaned = false; //por defeito, é falso?
+            Condition = condition;
+        }
+
+        public bool Loan()
+        {
+            // Joana
+            return true;
+        }
+
+        public bool Return()
+        {
+            // Joana
+            return true;
+        }
+    }
+}
---
+++ ProjectoGestaoBiblioteca/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace ProjectoGestaoBiblioteca
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var library = new Library("Group 3 Library", TimeSpan.FromDays(3));
+            Utils.NewConsole();
+            //José
+        }
+    }
+}
