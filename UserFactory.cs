2b2f5cc319a62a0b4a471d4e946974fdb537a87f
Viviane <viviane@unknown7ed7a79cf7c2.lan>
2025-04-16 00:49:26 +0100

    UserFactory

---
+++ UserFactory.cs
@@ -0,0 +1,15 @@
+﻿namespace ProjectoGestaoBiblioteca
+{
+    static class UserFactory
+    {
+        // deveria estar numa classe userFactory -> possui a logica de como criar users (ver o que é a factory design pattern)
+        public static User Create(string name, string username, string password, bool isAdmin = false, string? address = null, string? phone = null, bool hashPassword = true)
+        {
+            // hash password se não tiver hashed (registo)
+            // não fazer hash se já estiver (carregamento da BD no startup)
+            password = hashPassword ? password.GetHashCode().ToString() : password;
+            return new User(name, username, password, isAdmin, address, phone);
+        }
+    }
+}
+
